ed0ff24e8f1d7ef48d668a1b1418cc5848b75d44
I1538 <I1538@ISILLT10372.indium.com>
2019-06-21 18:46:32 +0530

    Added New files with Testng

---
+++ src/test/NewTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+  @Test
+  public void test1() 
+  {
+	  System.out.println("Welcome to Hello World program Test 1");
+  }
+  
+  @Test
+  public void test2() 
+  {
+	  System.out.println("Welcome to Hello World program Test 2");
+  }
+  
+  @Test
+  public void test3() 
+  {
+	  System.out.println("Welcome to Hello World program Test 3");
+  }
+  
+  @Test
+  public void test4() 
+  {
+	  System.out.println("Welcome to Hello World program Test 4");
+  }
+  
+  @Test
+  public void test5() 
+  {
+	  System.out.println("Welcome to Hello World program Test 5");
+  }
+  
+  @Test
+  public void test6() 
+  {
+	  System.out.println("Welcome to Hello World program Test 6");
+  }
+}
---
+++ src/test/NewTest2.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  System.out.println("Welcome to Hello World program");
+  }
+}
